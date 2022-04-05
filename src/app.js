8780d29920331459042759ef258e48cee457924d
benjalonso <alonsobenjaminlb@gmail.com>
2022-04-05 14:54:48 +0000

    Excuse generator developed on January

--- src/app.js
+++ src/app.js
@@ -1,11 +1,41 @@
 /* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = () => {
+  document.querySelector("#excuses").innerHTML = generateExcuse();
+  console.log("Hello little boy!");
+};
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let generateExcuse = () => {
+  let subject = ["My dog", "The bus", "My bathroom", "My mom", "My girlfriend"];
+  let verb = [
+    "has eaten",
+    "has puncture",
+    "has broken",
+    "told me that",
+    "took my laptop"
+  ];
+  let complement = [
+    "my homework",
+    "so I'm late",
+    "and I could'n go",
+    "so I got confused by the date",
+    "so I couldn't attend the meeting "
+  ];
+  let emoji = ["🥺", "😩", "😅", "😬", "😓"];
+
+  let subjectInd = Math.floor(Math.random() * subject.length);
+  let verbtInd = Math.floor(Math.random() * verb.length);
+  let complementtInd = Math.floor(Math.random() * complement.length);
+  let emojiInd = Math.floor(Math.random() * emoji.length);
+
+  return (
+    subject[subjectInd] +
+    " " +
+    verb[verbtInd] +
+    " " +
+    complement[complementtInd] +
+    " " +
+    emoji[emojiInd]
+  );
 };
